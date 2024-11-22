0c74c56071c225c4841b42e35002737cad6463c5
hackzilla-23 <russelkammoe9@gmail.com>
2024-11-22 10:29:59 +0100

    formulaire

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\RegiController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('login');
+// })->name('login');
+
+Route::get('/', [UserController::class, 'index'])->name('login');
+
+// Route::get('/register', function () {
+//     return view('register');
+// })->name('register');
+
+Route::get('/register', [RegiController::class, 'regi'])->name('register');
+
+// Route::get('/index', function () {
+//     return view('index');
+// });
