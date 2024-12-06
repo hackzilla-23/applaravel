31895b5b8809d12e9eab96a8a8f2203f4a6687ee
hackzilla-23 <russelkammoe9@gmail.com>
2024-12-06 09:55:35 +0100

    Update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\PersonneFormRequest;
 use App\Http\Requests\RequestLogs;
 use App\Http\Requests\RequestReset;
+use App\Mail\RegisterMail;
 use App\Models\Personne;
 use App\Models\Produit;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Password;
 
 class UserController extends Controller
@@ -43,15 +46,29 @@ class UserController extends Controller
         // ]);
 
         // dd($isvalid);
-        $newpersonne = Personne::create([
-            'nom' => $request->nom,
-            'prenom' => $request->prenom,
-            'age' => $request->age,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-        ]);
-        // dd($newpersonne);
-        return view('login', compact('newpersonne'));
+        try {
+            //code...
+            DB::transaction(function () use ($request) {
+                $newpersonne = Personne::create([
+                    'nom' => $request->nom,
+                    'prenom' => $request->prenom,
+                    'age' => $request->age,
+                    'email' => $request->email,
+                    'password' => bcrypt($request->password),
+                ]);
+                if ($newpersonne) {
+                    // Envoyer un email de confirmation
+                    Mail::to($newpersonne->email)->send(new RegisterMail($newpersonne));
+                }
+            });
+            // dd($newpersonne);
+            // return view('login', compact('newpersonne'));
+            return view('login');
+        } catch (\Throwable $th) {
+            //throw $th;
+            dd($th);
+            // return back();
+        }
     }
 
     public function regi()
@@ -77,8 +94,8 @@ class UserController extends Controller
         // Vérifier si l'utilisateur est authentifié
         if (Auth::guard('personnes')->check()) {
             $products = Produit::all();
-            return view('dashboard.product_dashboard')->with('allproducts' , $products);
-        }else {
+            return view('dashboard.product_dashboard')->with('allproducts', $products);
+        } else {
             sleep(1);
             return redirect()->route('login');
         }
@@ -156,9 +173,9 @@ class UserController extends Controller
         // Tentative de connexion
         $credentials = $request->only('email', 'password');
         $remember = $request->has('remember'); // Détermine si l'utilisateur a coché la case "se souvenir de moi"
-        
+
         // dd(Auth::guard('personnes')->attempt($credentials));
-        if (Auth::guard('personnes')->attempt($credentials)){
+        if (Auth::guard('personnes')->attempt($credentials)) {
             // Connexion réussie
             sleep(1);
             return redirect()->intended('dashboard');
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -167,7 +167,6 @@
         <!-- End of Sidebar Section -->
 
         @yield('main')
-        @yield('email')
     </div>
 
     {{-- pourcentage circle  --}}
@@ -373,7 +372,7 @@
             // Récupérer les éléments du DOM
             const editbtn = document.getElementById('editbtn' + id);
             const editProductModal = document.getElementById(id);
-            const closeEditBtn = document.getElementById('closeEditBtn'+id);
+            const closeEditBtn = document.getElementById('closeEditBtn' + id);
             // console.log(editbtn);
             // console.log(editProductModal);
             // editbtn.addEventListener('click', function() {
@@ -406,7 +405,7 @@
             // }
         }
         // Fonction pour réinitialiser les classes d'animation avant d'afficher le modal
-        
+
         // function resets(editProductModal) {
         //     // Réinitialiser les classes de translation avant de commencer une nouvelle animation
         //     // console.log(editProductModal);
