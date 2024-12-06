bed568dfb0c0aff3e29404ae1c9815d3038a8318
hackzilla-23 <russelkammoe9@gmail.com>
2024-12-06 10:25:14 +0100

    Update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Produit;
-use App\Models\Personne;
-use App\Mail\RegisterMail;
-use App\Http\Requests\RequestLogs;
-use Illuminate\Support\Facades\DB;
-use App\Http\Requests\RequestReset;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PersonneFormRequest;
+use App\Http\Requests\RequestLogs;
+use App\Http\Requests\RequestReset;
+use App\Mail\RegisterMail;
+use App\Models\Personne;
+use App\Models\Produit;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Password;
-use App\Http\Requests\PersonneFormRequest;
 
 class UserController extends Controller
 {
@@ -47,7 +47,7 @@ class UserController extends Controller
 
         // dd($isvalid);
         try {
-            $newpersonne = DB::transaction(function() use ($request){
+            $newpersonne = DB::transaction(function () use ($request) {
                 $user = Personne::create([
                     'nom' => $request->nom,
                     'prenom' => $request->prenom,
@@ -63,7 +63,7 @@ class UserController extends Controller
             });
 
             // Mail::to($request->email)->send(new RegisterMail ($request));
-            
+
             Mail::to($newpersonne->email)->send(new RegisterMail($newpersonne));
 
             return view('login', compact('newpersonne'));
@@ -72,7 +72,7 @@ class UserController extends Controller
             dd($th);
             // return back();
         }
-        
+
         // dd($newpersonne);
     }
 
